8b7fc37f2b99f09b4bf52a71f64d102513155223
Anoop Singh <anoop102910@gmail.com>
2024-03-13 12:49:19 +0530

    small change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,13 +7,13 @@ function Footer() {
         <a target="1" className="w-20" href="https://github.com/anoop102910?tab=repositories">
           <img  src="github.png" alt="" />
         </a>
-        <a target="2" className="w-20" href="https://wa.me/6388175878">
+        <a target="2" className="w-20" href="">
           <img src="./instagram.png" alt="" />
         </a>
-        <a target="3" className="w-20" href="https://instagram.com">
+        <a target="3" className="w-20" href="https://www.linkedin.com/in/anoop-singh-1a67ba252/">
           <img src="./linkedin.png" alt="" />
         </a>
-        <a target="4" className="w-20" href="https://www.linkedin.com/in/anoop-singh-1a67ba252/">
+        <a target="4" className="w-20" href="https://wa.me/6388175878">
           <img src="whatsapp.png" alt="" />
         </a>
       </div>
